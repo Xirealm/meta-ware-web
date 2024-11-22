743124a29d09218ef1a5461a2386cd19a4685168
Xirealm <1401443038@qq.com>
2024-11-22 22:05:47 +0800

    chore:项目构建

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/Login/LoginView.vue';
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/",
+      component: ()=> import("../layout/Layout.vue"),
+    },
+  ],
+});
+
+export default router
