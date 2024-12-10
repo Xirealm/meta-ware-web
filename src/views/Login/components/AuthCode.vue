57d720ac7ce81d0e69e6b9e4e44f09537b9fb934
Xirealm <1401443038@qq.com>
2024-12-10 08:11:36 +0800

    fix：注册页获取验证码按钮修复

--- src/views/Login/components/AuthCode.vue
+++ src/views/Login/components/AuthCode.vue
@@ -27,12 +27,12 @@ watch(code, (newVal, oldVal) => {
 <template>
     <div class="relative">
         <el-input placeholder="请输入验证码" size="large" v-model="code" maxlength="4"/>
-        <button 
+        <span 
             v-show = "!isSend"
-            class="absolute right-4 top-1/2 -translate-y-1/2 text-primary text-sm"
+            class="absolute right-4 top-1/2 -translate-y-1/2 text-primary text-sm cursor-pointer"
             @click="getCode">
             获取验证码
-        </button>
+        </span>
         <span 
             v-show="isSend"
             class="absolute right-4 top-1/2 -translate-y-1/2 text-primary text-sm">
