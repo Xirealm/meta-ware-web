bbe3fc77fcac1989e1ca11e491d8d8850acea0d5
Xirealm <1401443038@qq.com>
2024-12-16 17:21:34 +0800

    feat：顶部栏

--- src/layout/header/Header.vue
+++ src/layout/header/Header.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import IconMap from '@/assets/icons/IconMap.vue';
 </script>
 <template>
-    <header class="bg-white w-full h-[70px]">
-        
+    <header class="bg-white w-full flex items-center justify-between">
+        <div class="flex items-center">
+            <IconMap class="mx-3"/>
+            <div class="flex flex-col">
+                <span class="text-[#666666] text-sm">您当前的位置</span>
+                <span class="text-black">清华大学，海淀区</span>
+            </div>
+        </div>
+        <div class="flex gap-4 items-center mr-5">
+            <img src="@/assets/experts/expert1.png" class="rounded-full" alt="" width="36">
+            <span>用户名</span>
+        </div>
     </header>
 </template>
