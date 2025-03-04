b88e15fd1c92de71c150bd032a99f8007e5ab48e
Xirealm <1401443038@qq.com>
2025-03-04 14:02:08 +0800

    fix：路由调整为哈希模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import pinia from '@/stores/';
 import { useUserStore } from '@/stores/user';
 import LoginView from '../views/Login/LoginView.vue';
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/login",
