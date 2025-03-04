2a877fa9232d2ec84d587ea726cdfadc11990d9d
Xirealm <1401443038@qq.com>
2025-03-04 14:24:53 +0800

    fix：修复首页图片显示问题

--- src/views/Index/components/RightBottom.vue
+++ src/views/Index/components/RightBottom.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import ExpertData from './ExpertData.vue';
+import expert1 from '@/assets/experts/expert1.png'
+import expert2 from '@/assets/experts/expert2.png'
 
 const value = ref('过去24小时')
 
@@ -23,42 +25,42 @@ const expertsList = ref([
   {
     name: '李珊珊',
     base: '阳光心理咨询中心',
-    avatar: "/assets/experts/expert1.png",
+    avatar: expert1,
     allTimes: 10,
     currentTimes: 9,
   },
   {
     name: '王强',
     base: '安心心理咨询所',
-    avatar: "/assets/experts/expert2.png",
+    avatar: expert2,
     allTimes: 15,
     currentTimes: 6,
   },
   {
     name: '刘丽丽',
     base: '阳光心理咨询中心',
-    avatar: "/assets/experts/expert1.png",
+    avatar: expert1,
     allTimes: 12,
     currentTimes: 7,
   },
   {
     name: '李志强',
     base: '安心心理咨询所',
-    avatar: "/assets/experts/expert2.png",
+    avatar: expert2,
     allTimes: 8,
     currentTimes: 8,
   },
   {
     name: '王丽萍',
     base: '阳光心理咨询中心',
-    avatar: "/assets/experts/expert1.png",
+    avatar: expert1,
     allTimes: 10,
     currentTimes: 3,
   },
   {
     name: '张毅',
     base: '安心心理咨询所',
-    avatar: "/assets/experts/expert2.png",
+    avatar: expert2,
     allTimes: 24,
     currentTimes: 17,
   },
